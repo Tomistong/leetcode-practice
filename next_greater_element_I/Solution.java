848ed70a44d349488ba652b3d92a3f4832c9dd8a
YYSU <jim123820@gmail.com>
2019-03-12 17:57:47 +0800

    ADd Stack & Map

--- next_greater_element_I/Solution.java
+++ next_greater_element_I/Solution.java
@@ -1,28 +1,23 @@
-import java.util.Arrays;
+import java.util.Stack;
+import java.util.HashMap;
 
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int[] result = new int[nums1.length];      
-        Arrays.fill(result, -1);
+        int[] result = new int[nums1.length];
+        Stack<Integer> stack = new Stack<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
         
-        for (int i = 0; i < nums1.length; i++) {
-            int iLocationInNums2 = indexOf(nums2, nums1[i]);
-            for (int j = iLocationInNums2; j < nums2.length; j++) {
-                if (nums2[j] > nums1[i]) {
-                    result[i] = nums2[j];
-                    break;
-                } 
+        for (int num : nums2) {
+            while (!stack.isEmpty() && stack.peek() < num) {
+                map.put(stack.pop(), num);
             }
+            stack.push(num);
         }
+        
+        for (int i = 0; i < result.length; i++) {
+            result[i] = map.getOrDefault(nums1[i], -1);
+        }
+        
         return result;
-    }
-    
-    public int indexOf(int[] array, int number) {
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == number) {
-                return i;
-            }    
-        }        
-        return -1;
     }
 }
