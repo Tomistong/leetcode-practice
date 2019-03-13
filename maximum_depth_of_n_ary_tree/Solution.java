2299d7d9745841de78ba7b5b5e67baac30541dd1
YYSU <jim123820@gmail.com>
2019-03-13 22:20:21 +0800

    Add maximun depth

---
+++ maximum_depth_of_n_ary_tree/Solution.java
@@ -0,0 +1,40 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val,List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+import java.util.Queue;
+import java.util.LinkedList;
+
+class Solution {
+    public int maxDepth(Node root) {
+        Queue<Node> queue = new LinkedList<>();
+        int depth = 0;
+        
+        if (root == null) return 0;
+        queue.offer(root);
+        
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            
+            for (int i = 0; i < size; i ++) {
+                Node current = queue.poll();
+                for (Node child : current.children) {
+                    queue.offer(child);
+                }
+            }
+            depth++;
+        }
+        return depth;
+    }
+}
